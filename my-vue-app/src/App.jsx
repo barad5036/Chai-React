49be7e6b2bf2417aa87458af02d44f35eae6dcf1
barad5036 <barad5036@gmail.com>
2025-07-25 17:26:48 +0530

    last final changes..

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,16 +1,12 @@
+// src/App.jsx
+import { useState } from "react";
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-// import "./index.css";  ← this line removed
-import App from "./App.jsx";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <h1>
-      Hello,Welcome to the Vite-Project
-    </h1>
+    <h1>Hello, Welcome to the Vite-Project</h1>
   );
 }
 
-export default App
+export default App;
