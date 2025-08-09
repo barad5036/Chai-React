9aaa79ae8a560fce4b931966ed83803828acb636
barad5036 <barad5036@gmail.com>
2025-08-09 20:43:25 +0530

    made a learning file as learning.jsx and given it access as import in the App.jsx because in the Main.jsx the app.jsx is rendered...This is the quality of react

---
+++ my-vue-app/src/learning.jsx
@@ -0,0 +1,14 @@
+function Learning_react(){
+    return (
+        <h2>I am learning React as, how to add a new file and export in the same file with the syntax export default Learning_react - name of the function and 
+            calling the function in the rendered file ie let say we have render App.jsx in the main.jsx as createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+In this manner we render the app and inside the app we call this file i.e Learning_react, as import Learning_react from "./learning.jsx"
+Now, inside the App function you will written learning function as inside the arrow bracket the name of the function i.e Learning_react,and hence your
+code inside will automatically run where it is being executed.
+        </h2>
+    )
+}
