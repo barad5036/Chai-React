d502d2a2816d710429ef8f47aa6bb31edee5f346
barad5036 <barad5036@gmail.com>
2025-08-09 20:47:02 +0530

    file imported

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import { useState } from "react";
 import Chai from "./chai"
+import Learning_react from "./learning"
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <h1>Hello, Welcome to the Vite-Project</h1>
+    <Learning_react/>
     <Chai/>
     <h3>Chai and react code imported in the app.jsx and the app.jsx is rendered in the main.jsx in the vite</h3>
     </>
